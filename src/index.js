d0013a39016c96cb49e22c4a47168ae2f6db5ef7
venugopal <pvgopal@vivifyhealthcare.com>
2023-12-05 09:44:59 +0530

    update

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
+import reactlogo from "./logo.png";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+
 //create a nested Element using React.createElement(h1,h2,h3 inside a div with class "title")
 const Header = React.createElement(
     "div",{className: "title", key:"title"},
@@ -29,7 +33,7 @@ const Header2 = () => {
       </div>
   )
 };
-//ReactDOM.render(<Header2/>,document.getElementById("root"))
+ReactDOM.render(<Header2/>,document.getElementById("root"))
 
 
 //pass attributes into the tag in jsx
@@ -72,13 +76,13 @@ const Header6 = () => {
   return (
     <component className="component">
       <div className="logo">
-        <img src="logo.png" alt="logo"/>
+      <img src={reactlogo} height="30px" width="40x"/>
       </div>
-      < div className="new">
+      <div className="new">
         <input type="text" placeholder="search here"/>
       </div>
       <div className='icon'>
-        <img src="icon.png" alt="icon"/>
+         <svg xmlns="http://www.w3.org/2000/svg" height="100px" width="50px" viewBox="0 0 576 512"><path d="M549.7 124.1c-6.3-23.7-24.8-42.3-48.3-48.6C458.8 64 288 64 288 64S117.2 64 74.6 75.5c-23.5 6.3-42 24.9-48.3 48.6-11.4 42.9-11.4 132.3-11.4 132.3s0 89.4 11.4 132.3c6.3 23.7 24.8 41.5 48.3 47.8C117.2 448 288 448 288 448s170.8 0 213.4-11.5c23.5-6.3 42-24.2 48.3-47.8 11.4-42.9 11.4-132.3 11.4-132.3s0-89.4-11.4-132.3zm-317.5 213.5V175.2l142.7 81.2-142.7 81.2z"/></svg>
       </div>
     
     </component>
