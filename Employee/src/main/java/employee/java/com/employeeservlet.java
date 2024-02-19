f963337f2e8a91817e44726813e568853159f9a4
VenuGopal-46 <149860017+VenuGopal-46@users.noreply.github.com>
2024-02-19 14:10:31 +0530

    updated

--- Employee/src/main/java/employee/java/com/employeeservlet.java
+++ Employee/src/main/java/employee/java/com/employeeservlet.java
@@ -1,11 +1,12 @@
 package employee.java.com;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 
 public class employeeservlet extends HttpServlet {
